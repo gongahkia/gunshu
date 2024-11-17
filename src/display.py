116f531a13eeae64d2a385a2e3e8fe42ef08232e
gongahkia <angryapplegravy@gmail.com>
2024-11-17 18:55:07 +0800

    feat(app): noted

--- src/display.py
+++ src/display.py
@@ -145,6 +145,29 @@ def quit_display():
     print("exiting pygame now...")
 
 
+# EFFECTS
+
+
+def load_effect_frames(sprite_filepath, frame_count):
+    """
+    to load in particle effects
+    """
+    sprite_sheet = pygame.image.load(sprite_filepath).convert_alpha()
+    frame_width = 80
+    frame_height = 80
+    frames = []
+    for i in range(frame_count):
+        row = i // 4
+        col = i % 4
+        frame = sprite_sheet.subsurface(
+            pygame.Rect(
+                col * frame_width, row * frame_height, frame_width, frame_height
+            )
+        )
+        frames.append(frame)
+    return frames
+
+
 # USER CONTROLS
 
 
