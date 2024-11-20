792c43f8f1d0303ef46fb74c429d7278541ade27
gongahkia <angryapplegravy@gmail.com>
2024-11-20 23:09:53 +0800

    feat(app): moving bullshit around like its free teehee

--- archive/placeholder_sprite/README.md
+++ archive/placeholder_sprite/README.md
@@ -1,6 +1,6 @@
 # Note
 
-Just placeholder sprites, to be moved to `./archive/sprite/` after actual sprites are loaded in.
+Placeholder sprites used early in testing.
 
 ## Credit
 
---
+++ src/game/powerup_bar.py
@@ -0,0 +1,1 @@
+# FUA add a function that takes in the player's current inventory or powerup as an array and displays their powerup sprites, including a numbered count of each powerup
