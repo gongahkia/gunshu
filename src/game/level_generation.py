405c2be3f35c4438af79d9242d8f7bdd724b973c
gongahkia <angryapplegravy@gmail.com>
2024-11-21 23:08:12 +0800

    feat(app): wfc

---
+++ src/game/level_generation.py
@@ -0,0 +1,5 @@
+# FUA add wave function collapse
+
+# Implement wave function collapse in GUNSHU from here https://youtu.be/TO0Tx3w5abQ?si=yqSf580MlUwQooCA as a file and find a python library
+
+# Need to think of a way to store the generated level data as well if the player needs to save level progress
